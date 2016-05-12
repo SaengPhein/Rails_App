8d8e8f546e4315d46e85a1468f458b836a2d8065
Anahit Sarao <indianvip60@gmail.com>
2016-05-11 22:32:58 -0700

    Added SCSS Files, Fixed Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  
-  get "About" => "pages#about", as: :about
-  get "Contact" => "pages#contact", as: :contact
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
